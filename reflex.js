406f67cab8127aaff51807c5c762db7c6e2b4b9c
lemkeprzemyslaw <lemkeprzemyslaw@gmail.com>
2019-07-13 17:45:11 +0200

    refactor css code

--- reflex.js
+++ reflex.js
@@ -2,7 +2,7 @@ import createBoard from './components/createBoard.js';
 
 const gameState = {
   boardSize: 5,
-  gameTime: 60000,
+  gameTime: 60,
   health: 3,
   points: 0,
 };
@@ -10,7 +10,15 @@ const gameState = {
 createBoard('board', gameState.boardSize);
 
 const allTiles = document.querySelectorAll('.tile');
+const healthIndicator = document.querySelector('#health');
+const pointsIndicator = document.querySelector('#points');
+const timeIndicator = document.querySelector('#time');
 
+const showIndicator = (indicatorId, indicatorName, indicatorValue) => indicatorId.textContent = indicatorName + ': ' + indicatorValue;
+
+showIndicator(healthIndicator, 'Życie', gameState.health);
+showIndicator(pointsIndicator, 'Punkty', gameState.points);
+showIndicator(timeIndicator, 'Pozostały czas', gameState.gameTime);
 
 const randomTile = allTiles[Math.floor(Math.random() * Math.pow(gameState.boardSize, 2))];
 
